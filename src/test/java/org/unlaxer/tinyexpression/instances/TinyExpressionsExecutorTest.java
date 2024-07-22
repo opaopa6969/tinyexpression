7be9f948d7cd7d0fa2cf69cdf7db7a0aea01b997
opa <opaopa6969@gmail.colm>
2024-07-23 07:48:32 +0900

    ~TinyExpressionsExecutorTest~

---
+++ src/test/java/org/unlaxer/tinyexpression/instances/TinyExpressionsExecutorTest.java
@@ -0,0 +1,14 @@
+package org.unlaxer.tinyexpression.instances;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TinyExpressionsExecutorTest {
+
+  @Test
+  public void test() {
+    fail("Not yet implemented");
+  }
+
+}
