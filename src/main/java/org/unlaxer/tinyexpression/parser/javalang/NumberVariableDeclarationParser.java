99fb75d7780b091c03a11fb9bbbbc981d9b6805b
opa <opaopa6969@gmail.com>
2025-03-16 17:07:16 +0900

    fix conflicts

--- src/main/java/org/unlaxer/tinyexpression/parser/javalang/NumberVariableDeclarationParser.java
+++ src/main/java/org/unlaxer/tinyexpression/parser/javalang/NumberVariableDeclarationParser.java
@@ -6,7 +6,7 @@ import org.unlaxer.Tag;
 import org.unlaxer.parser.Parser;
 import org.unlaxer.tinyexpression.parser.ExpressionType;
 import org.unlaxer.tinyexpression.parser.ExpressionTypes;
-import org.unlaxer.tinyexpression.parser.numbertype.NumberSetterParser;
+import org.unlaxer.tinyexpression.parser.NumberSetterParser;
 
 @SuppressWarnings("serial")
 public class NumberVariableDeclarationParser extends AbstractVariableDeclarationParser{
