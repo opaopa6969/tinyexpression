a436daa639530a51714f2654a6ee9020d1ee73a3
opaopa6969 <opaopa6969@gmail.com>
2026-09-19 18:49:45 +0900

    test: pin Java emitter Unicode and slice boundary behavior

--- src/test/java/org/unlaxer/tinyexpression/evaluator/javacode/DslGeneratedAstJavaEmitterTest.java
+++ src/test/java/org/unlaxer/tinyexpression/evaluator/javacode/DslGeneratedAstJavaEmitterTest.java
@@ -2,6 +2,8 @@ package org.unlaxer.tinyexpression.evaluator.javacode;
 
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.fail;
 
 import java.lang.reflect.Method;
@@ -17,8 +19,17 @@ public class DslGeneratedAstJavaEmitterTest {
 
   @Test
   public void sliceIndicesRemainRawJavaThroughOwnedParseResult() {
-    String[] formulas = {"'gateman'[::-1]", "'1a2b3'[::2]", "'abcdef'[1:4]"};
-    String[] expected = {"nametag", "123", "bcd"};
+    // This fixes the Java CodePoint Slicer contract, not the separate AST evaluator's UTF-16 rules.
+    String[] formulas = {
+        "'gateman'[::-1]", "'1a2b3'[::2]", "'abcdef'[1:4]",
+        "'😀a'[1:2]", "'😀a'[0:1]", "'😀a'[::-1]", "'abcdef'[::0]",
+        "'abcdef'[-3:-1]", "'abcdef'[2:]", "'abcdef'[:3]", "'abcdef'[:]",
+        "'abcdef'[3:3]", "'abcdef'[1:4:-1]", "'abcdef'[(1+1):5]"
+    };
+    String[] expected = {
+        "nametag", "123", "bcd", "a", "😀", "a😀", "",
+        "de", "cdef", "abc", "abcdef", "", "dcb", "cde"
+    };
     var types = new SpecifiedExpressionTypes(ExpressionTypes.string, ExpressionTypes._float);
     ClassLoader loader = Thread.currentThread().getContextClassLoader();
     for (int i = 0; i < formulas.length; i++) {
@@ -30,6 +41,32 @@ public class DslGeneratedAstJavaEmitterTest {
       var calculator = CalculatorCreatorRegistry.forBackend(ExecutionBackend.DSL_JAVA_CODE)
           .create(new Source(formulas[i]), name, types, loader);
       assertEquals(formulas[i], expected[i], calculator.apply(CalculationContext.newConcurrentContext()));
+    }
+  }
+
+  @Test
+  public void outOfRangeSliceBoundsRemainExplicitRuntimeFailures() {
+    String[] formulas = {"'abc'[0:4]", "'abc'[-4:2]", "'abc'[2:1]", "'abc'[4:]"};
+    var types = new SpecifiedExpressionTypes(ExpressionTypes.string, ExpressionTypes._float);
+    ClassLoader loader = Thread.currentThread().getContextClassLoader();
+    for (int i = 0; i < formulas.length; i++) {
+      String name = "OwnedSliceBoundsProbe" + i;
+      assertTrue(formulas[i], DslGeneratedAstJavaEmitter.tryEmit(
+          name, new Source(formulas[i]), types, loader).isPresent());
+      var calculator = CalculatorCreatorRegistry.forBackend(ExecutionBackend.DSL_JAVA_CODE)
+          .create(new Source(formulas[i]), name, types, loader);
+      assertThrows(formulas[i], IndexOutOfBoundsException.class,
+          () -> calculator.apply(CalculationContext.newConcurrentContext()));
+    }
+  }
+
+  @Test
+  public void malformedIndexSyntaxIsRejectedBeforeJavaEmission() {
+    var types = new SpecifiedExpressionTypes(ExpressionTypes.string, ExpressionTypes._float);
+    ClassLoader loader = Thread.currentThread().getContextClassLoader();
+    for (String formula : new String[] {"'abc'[oops:2]", "'abc'[1+:2]"}) {
+      assertFalse(formula, DslGeneratedAstJavaEmitter.tryEmit(
+          "OwnedSliceInvalidProbe", new Source(formula), types, loader).isPresent());
     }
   }
 
